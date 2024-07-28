2e81a6821b7b7a30e25ac2a633b69c92f477fba3
Fabio-R-Moraes <fmoraes05@outlook.com>
2024-07-27 21:30:03 -0300

    Adicionando rotas

--- estudo/view.py
+++ estudo/view.py
@@ -1,6 +1,10 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
+@app.route('/nova/')
+def novapage():
+    return "Esta é uma nova página!!!"
