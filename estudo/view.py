d412f73c9bcfee046478814ccbf8ded7817ae5b4
Fabio-R-Moraes <fmoraes05@outlook.com>
2024-07-28 16:02:31 -0300

    Criado banco de dados e tabela

--- estudo/view.py
+++ estudo/view.py
@@ -3,7 +3,12 @@ from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    usuario = 'Fabio'
+    context = {
+        'idade':40,
+        'cidade':'Curitiba',
+    }
+    return render_template("index.html", usuario=usuario, context=context)
 
 @app.route('/nova/')
 def novapage():
